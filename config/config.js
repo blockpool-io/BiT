d4a6653e053dc5d7b000dd952e24fb31595fa191
Andrew Cox <roo@rhmg.co.uk>
2018-02-19 11:27:32 +0200

    change blue

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@ module.exports = {
     apipublickey : 'd9ce56ce-6650-499d-913c-550d9be62d75',
     apiprivatekey : 'agent upon clinic fame tomorrow lab fade muffin payment fox quick damp', // DO NOT EVER REVEAL THIS IN PUBLIC CONFIG OR REPOSITORIES!!!
 
-    urlToBit : 'http://localhost:8080'
+    urlToBit : process.env.urlToBit || 'http://localhost:8080'
 };
