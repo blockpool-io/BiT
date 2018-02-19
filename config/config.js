2b636f411f864829db0f14d7abeef126757f9cb0
Andrew Cox <roo@rhmg.co.uk>
2018-02-19 11:50:59 +0200

    add config for api keys

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    apipublickey : 'd9ce56ce-6650-499d-913c-550d9be62d75',
-    apiprivatekey : 'agent upon clinic fame tomorrow lab fade muffin payment fox quick damp', // DO NOT EVER REVEAL THIS IN PUBLIC CONFIG OR REPOSITORIES!!!
+    apipublickey : process.env.apipublickey || 'd9ce56ce-6650-499d-913c-550d9be62d75',
+    apiprivatekey : process.env.apiprivatekey || 'agent upon clinic fame tomorrow lab fade muffin payment fox quick damp', // DO NOT EVER REVEAL THIS IN PUBLIC CONFIG OR REPOSITORIES!!!
 
     urlToBit : process.env.urlToBit || 'http://localhost:8080'
 };
