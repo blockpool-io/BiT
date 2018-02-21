e45f48823b4845b68a6d3ed433697110b531eb36
Andrew Cox <roo@rhmg.co.uk>
2018-02-21 15:50:37 +0200

    associate a user with a reward location

---
+++ routes/trusted/locations/associate_user_to_location.js
@@ -0,0 +1,54 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+const config = require('../../../config/config');
+const handleBiTHeaders = require('../../../headerhandler');
+
+router.post('/', (req, res) => {
+  let bitid = req.body.bitid;
+  let locationid = req.body.locationid;
+  if (!bitid || !locationid) {
+    return res.status(400).json({
+      success : false,
+      message : "missing required fields"
+    });
+  }
+  delete req.body.locationid;
+
+  handleBiTHeaders.hashKey(config.apiprivatekey)
+  .then((apihashedkey) => {
+    let params = {
+      url : config.urlToBit + '/organisations/locations/' + locationid + '/associate',
+      headers : {
+        apipublickey : config.apipublickey,
+        apihashedkey : apihashedkey
+      },
+      json : true,
+      body : req.body
+    };
+
+    request.post(params, (error, response, body) => {
+      if (error) {
+        let statusCode = 500;
+        if (response) {
+          statusCode = response.statusCode;
+        }
+        return res.status(statusCode).json({
+          success : false,
+          message: error,message
+        });
+      } else {
+        return res.status(response.statusCode).json(body);
+      }
+    });
+  })
+  .catch((err) => {
+    return res.status(401).json({
+      success : false,
+      message: err.message
+    });
+  });
+});
+
+module.exports = router;
--- routes/trusted/locations/index.js
+++ routes/trusted/locations/index.js
@@ -4,4 +4,5 @@ const express = require('express');
 var app = module.exports = express();
 
 app.use('/add_simple_rewarding_location', require('./add_simple_rewarding_location'));
+app.use('/associate_user_to_location', require('./associate_user_to_location'));
 
