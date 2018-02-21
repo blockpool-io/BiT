40276616e969c4d08f1256b7537291e51b4a3766
Andrew Cox <roo@rhmg.co.uk>
2018-02-21 10:30:32 +0200

    query balance

--- routes/trusted/index.js
+++ routes/trusted/index.js
@@ -5,6 +5,7 @@ var app = module.exports = express();
 
 app.use('/associate', require('./associate'));
 app.use('/query_account', require('./query_account'));
+app.use('/query_balance', require('./query_balance'));
 app.use('/create_wallet', require('./create_wallet'));
 app.use('/send_funds', require('./send_funds'));
 
