705735f64625019276ba962f102c85746bc1bbe7
Andrew Cox <roo@rhmg.co.uk>
2018-02-22 20:28:47 +0200

    calculate fee for paas

--- routes/paas/wallets/index.js
+++ routes/paas/wallets/index.js
@@ -6,4 +6,5 @@ var app = module.exports = express();
 app.use('/create_wallet', require('./create_wallet'));
 app.use('/query_balance', require('./query_balance'));
 app.use('/query_account', require('./query_account'));
+app.use('/calculate_fee', require('./calculate_fee'));
 app.use('/send_funds', require('./send_funds'));
