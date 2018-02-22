4eb5e88ca8d1d6e43dfa02bbb36aad3b1b4fcd55
Andrew Cox <roo@rhmg.co.uk>
2018-02-22 18:00:45 +0200

    calculate fee

---
+++ routes/trusted/wallets/calculate_fee.js
@@ -0,0 +1,83 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+const config = require('../../../config/config');
+const handleBiTHeaders = require('../../../headerhandler');
+
+router.post('/', (req, res) => {
+  let ticker = req.body.ticker;
+  let amount = req.body.amount;
+  let originbitid = req.body.originbitid;
+  let originwalletid = req.body.originwalletid;
+
+  if (!ticker || !amount || !originbitid || !originwalletid) {
+    return res.status(400).json({
+      success : false,
+      message : "missing required fields"
+    });
+  }
+
+  let destinationbitid = req.body.destinationbitid;
+  let destinationwalletid = req.body.destinationwalletid;
+
+  let destinationaddress = req.body.destinationaddress;
+
+  let destination = {};
+
+  if (destinationaddress) {
+    destination.address = destinationaddress;
+  } else if (destinationbitid && destinationwalletid) {
+    destination.bitid = destinationbitid;
+    destination.walletid = destinationwalletid;
+  } else {
+    return res.status(400).json({
+      success : false,
+      message : "missing required fields"
+    });
+  }
+
+
+  handleBiTHeaders.hashKey(config.apiprivatekey)
+  .then((apihashedkey) => {
+    let params = {
+      url : config.urlToBit + '/organisations/transactions/' + ticker + '/calculatefee',
+      headers : {
+        apipublickey : config.apipublickey,
+        apihashedkey : apihashedkey
+      },
+      json : true,
+      body : {
+        amount : amount,
+        origin : {
+          bitid : originbitid,
+          walletid : originwalletid
+        },
+        destination : destination
+      }
+    };
+
+    request.put(params, (error, response, body) => {
+      if (error) {
+        let statusCode = 500;
+        if (response) {
+          statusCode = response.statusCode;
+        }
+        return res.status(statusCode).json({
+          success : false,
+          message: error.message
+        });
+      } else {
+        return res.status(response.statusCode).json(body);
+      }
+    });
+  })
+  .catch((err) => {
+    return res.status(401).json({
+      success : false,
+      message: err.message
+    });
+  });
+});
+
+module.exports = router;
--- routes/trusted/wallets/index.js
+++ routes/trusted/wallets/index.js
@@ -7,5 +7,6 @@ app.use('/associate', require('./associate'));
 app.use('/query_account', require('./query_account'));
 app.use('/query_balance', require('./query_balance'));
 app.use('/create_wallet', require('./create_wallet'));
+app.use('/calculate_fee', require('./calculate_fee'));
 app.use('/send_funds', require('./send_funds'));
 
