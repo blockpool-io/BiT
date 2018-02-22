cbb13e24ca2a56d61c5ef479e5b2eaa13fff4acf
Andrew Cox <roo@rhmg.co.uk>
2018-02-22 20:04:32 +0200

    send funds via paas

--- routes/paas/wallets/index.js
+++ routes/paas/wallets/index.js
@@ -6,3 +6,4 @@ var app = module.exports = express();
 app.use('/create_wallet', require('./create_wallet'));
 app.use('/query_balance', require('./query_balance'));
 app.use('/query_account', require('./query_account'));
+app.use('/send_funds', require('./send_funds'));
---
+++ routes/paas/wallets/send_funds.js
@@ -0,0 +1,62 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+const config = require('../../../config/config');
+const handleBiTHeaders = require('../../../headerhandler');
+
+router.post('/', (req, res) => {
+  let ticker = req.body.ticker;
+  let amount = req.body.amount;
+  let originwalletuniqueidentifier = req.body.originwalletuniqueidentifier;
+  let destinationwalletuniqueidentifier = req.body.destinationwalletuniqueidentifier;
+
+  if (!ticker || !amount || !originwalletuniqueidentifier || !destinationwalletuniqueidentifier) {
+    return res.status(400).json({
+      success : false,
+      message : "missing required fields"
+    });
+  }
+  let comment = req.body.comment;
+
+  handleBiTHeaders.hashKey(config.apiprivatekey)
+  .then((apihashedkey) => {
+    let params = {
+      url : config.urlToBit + '/transactions/' + ticker,
+      headers : {
+        apipublickey : config.apipublickey,
+        apihashedkey : apihashedkey
+      },
+      json : true,
+      body : {
+        amount : amount,
+        comment : comment,
+        originwalletuniqueidentifier : originwalletuniqueidentifier,
+        destinationwalletuniqueidentifier : destinationwalletuniqueidentifier
+      }
+    };
+
+    request.post(params, (error, response, body) => {
+      if (error) {
+        let statusCode = 500;
+        if (response) {
+          statusCode = response.statusCode;
+        }
+        return res.status(statusCode).json({
+          success : false,
+          message: error.message
+        });
+      } else {
+        return res.status(response.statusCode).json(body);
+      }
+    });
+  })
+  .catch((err) => {
+    return res.status(401).json({
+      success : false,
+      message: err.message
+    });
+  });
+});
+
+module.exports = router;
