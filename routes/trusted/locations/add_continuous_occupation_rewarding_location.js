c67e196d353704006d4c7ab24f32d125adf684d7
Andrew Cox <roo@rhmg.co.uk>
2018-03-02 10:15:49 +0200

    add coninuous occupation based rewarding

---
+++ routes/trusted/locations/add_continuous_occupation_rewarding_location.js
@@ -0,0 +1,63 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+const config = require('../../../config/config');
+const handleBiTHeaders = require('../../../headerhandler');
+
+router.post('/', (req, res) => {
+  let ticker = req.body.ticker;
+  let locationlabel = req.body.locationlabel;
+  let rewardingbitid = req.body.rewardingbitid;
+  let rewardingwalletid = req.body.rewardingwalletid;
+  let amount = req.body.amount;
+  let latitude = req.body.latitude;
+  let longitude = req.body.longitude;
+  let proximity = req.body.proximity;
+  let occupationperiod  = req.body.occupationperiod;
+  let disabled = req.body.disabled;
+  if (!ticker || !locationlabel || !rewardingbitid || !rewardingwalletid || !amount || !latitude || !longitude || !proximity || !occupationperiod || disabled === undefined) {
+    return res.status(400).json({
+      success : false,
+      message : "missing required fields"
+    });
+  }
+
+  delete req.body.ticker;
+  
+  handleBiTHeaders.hashKey(config.apiprivatekey)
+  .then((apihashedkey) => {
+    let params = {
+      url : config.urlToBit + '/organisations/locations/' + ticker,
+      headers : {
+        apipublickey : config.apipublickey,
+        apihashedkey : apihashedkey
+      },
+      json : true,
+      body : req.body
+    };
+
+    request.post(params, (error, response, body) => {
+      if (error) {
+        let statusCode = 500;
+        if (response) {
+          statusCode = response.statusCode;
+        }
+        return res.status(statusCode).json({
+          success : false,
+          message: error,message
+        });
+      } else {
+        return res.status(response.statusCode).json(body);
+      }
+    });
+  })
+  .catch((err) => {
+    return res.status(401).json({
+      success : false,
+      message: err.message
+    });
+  });
+});
+
+module.exports = router;
--- routes/trusted/locations/index.js
+++ routes/trusted/locations/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 var app = module.exports = express();
 
 app.use('/add_simple_rewarding_location', require('./add_simple_rewarding_location'));
+app.use('/add_continuous_occupation_rewarding_location', require('./add_continuous_occupation_rewarding_location'));
 app.use('/associate_user_to_location', require('./associate_user_to_location'));
 app.use('/list_locations', require('./list_locations'));
 app.use('/query_location', require('./query_location'));
