aee4320d5736c78d7757c86e29cfffeb842d24fe
Andrew Cox <roo@rhmg.co.uk>
2018-06-22 08:01:24 +0300

    adding getting of qr code and improving merchant docs

---
+++ routes/paas/merchant/get_qr_code.js
@@ -0,0 +1,53 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+const config = require('../../../config/config');
+const handleBiTHeaders = require('../../../headerhandler');
+
+router.post('/', (req, res) => {
+  let ticker = req.body.ticker;
+  let transactionId = req.body.transactionId;
+
+  if (!ticker || !transactionId) {
+    return res.status(400).json({
+      success : false,
+      message : "missing required fields"
+    });
+  }
+
+  handleBiTHeaders.hashKey(config.apiprivatekey)
+  .then((apihashedkey) => {
+    let params = {
+      url : config.urlToBit + '/merchant/' + ticker + '/makepayment/' + transactionId,
+      headers : {
+        apipublickey : config.apipublickey,
+        apihashedkey : apihashedkey
+      },
+      json : true,
+    };
+
+    request.get(params, (error, response, body) => {
+      if (error) {
+        let statusCode = 500;
+        if (response) {
+          statusCode = response.statusCode;
+        }
+        return res.status(statusCode).json({
+          success : false,
+          message: error.message
+        });
+      } else {
+        return res.status(response.statusCode).json(body);
+      }
+    });
+  })
+  .catch((err) => {
+    return res.status(401).json({
+      success : false,
+      message: err.message
+    });
+  });
+});
+
+module.exports = router;
--- routes/paas/merchant/index.js
+++ routes/paas/merchant/index.js
@@ -4,4 +4,5 @@ const express = require('express');
 var app = module.exports = express();
 
 app.use('/add_expected_payment', require('./add_expected_payment'));
+app.use('/get_qr_code', require('./get_qr_code'));
 
